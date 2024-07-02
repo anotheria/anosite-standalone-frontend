b2d29b38dacb3925526bf6b85778ca81b6aaf0b8
saenq <vsaenko4@gmail.com>
2024-07-02 17:08:20 +0300

    Setup basic project infrastructure

---
+++ src/components/layouts/BasicLayout.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container mx-auto px-4 min-h-screen flex flex-col">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="sass"></style>
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import checker from 'vite-plugin-checker'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    checker({
+      vueTsc: {
+        tsconfigPath: './tsconfig.app.json',
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
