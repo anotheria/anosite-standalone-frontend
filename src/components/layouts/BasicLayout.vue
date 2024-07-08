a8debf2671f467b2da8d24452b497bfddefda8fb
saenq <vsaenko4@gmail.com>
2024-07-08 12:08:38 +0300

    refactor: Remove unused layout components and their associated styles

--- src/components/layouts/BasicLayout.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div class="container mx-auto px-4 min-h-screen flex flex-col">
-    <slot></slot>
-  </div>
-</template>
-
-<style scoped lang="sass"></style>
